995b16a59e634fd1430b8b764352f84c42f5ccc4
st20080625 <soichiro5180@gmail.com>
2025-01-06 20:04:52 +0900

    #

--- main_pc.py
+++ main_pc.py
@@ -5,13 +5,16 @@ addr = '192.168.1.40'
 port = 5003
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-cam = subprocess.run(['python', 'cam_receive.py'])
-controller = subprocess.run(['python', 'controll_client.py'])
-render_3d = subprocess.run(['python', 'receive_angle.py'])
+cam = subprocess.Popen(['python', 'cam_receive.py'])
+controller = subprocess.Popen(['python', 'controll_client.py'])
+render_3d = subprocess.Popen(['python', 'receive_angle.py'])
 running = True
 while running:
     if win32api.GetAsyncKeyState(0x0D) < 0:
-        sock.sendto(0,(addr, port))
+        sock.sendto(b'0',(addr, port))
         running = False
     else:
         pass
+cam.terminate()
+controller.terminate()
+render_3d.terminate()
--- main_raspi.py
+++ main_raspi.py
@@ -5,9 +5,9 @@ port = 5003
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((addr, port))
 
-cam = subprocess.run(['python', 'cam.py'])
-controller = subprocess.run(['python', 'controll.py'])
-angle = subprocess.run(['python', 'send_angle.py'])
+cam = subprocess.Popen(['python', 'cam.py'])
+controller = subprocess.Popen(['python', 'motor.py'])
+angle = subprocess.Popen(['python', 'send_angle.py'])
 running = True
 while running:
     data, addr = sock.recvfrom(1024)
@@ -15,3 +15,6 @@ while running:
         running = False
     else:
         pass
+cam.terminate()
+controller.terminate()
+angle.terminate()
