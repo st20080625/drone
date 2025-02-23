e0f269ee972989cdf03741de2366f5ad52957a38
st20080625 <soichiro5180@gmail.com>
2025-02-24 01:19:52 +0900

    #

--- raspi/send_angle.py
+++ raspi/send_angle.py
@@ -119,12 +119,26 @@ def calibrate_compass():
     y0 = origin[1]
     return x0, y0
 
+def offset_angle_z(offset_cx, offset_cy):
+    cx = read_compass_sensor(MAGNET_XOUT)
+    cy = read_compass_sensor(MAGNET_YOUT)
+
+    cx_gauss = (cx * LSB_compass) / LSB_GAUSS_XY
+    cy_gauss = (cy * LSB_compass) / LSB_GAUSS_XY
+
+    cx_gauss -= offset_cx
+    cy_gauss -= offset_cy
+
+    offset_angle_z = np.arctan2(cy_gauss, cx_gauss)*180/np.py
+    return offset_angle_z
 angle_x, angle_y, angle_z = 0.0, 0.0, 0.0
 current_time = time.time()
 
 offset_gx, offset_gy, offset_gz = calibrate_gyro()
+time.sleep(1)
 offset_cx, offset_cy = calibrate_compass()
-
+time.sleep(5)
+offset_angle_z = offset_angle_z(offset_cx, offset_cy)
 while True:
     try:
         # Gyro
@@ -176,7 +190,9 @@ while True:
 
     angle_x = alpha * (angle_x + gx * delta_t_r) + (1 - alpha) * acc_angle_x
     angle_y = alpha * (angle_y + gy * delta_t_r) + (1 - alpha) * acc_angle_y
-    angle_z = np.arctan2(cy_gauss, cx_gauss)*180/np.pi
+    angle_z = np.arctan2(cy_gauss, cx_gauss)*180/np.pi-offset_angle_z
+    if angle_z < 0:
+        angle_z += 360
 
     send_data = json.dumps({'angle_x': angle_x, 'angle_y': angle_y, 'angle_z': angle_z})
     sock.sendto(send_data.encode(),(ip_addr, port))
